60dd866eaf6eea3c9d27bfaa8bee9908722876ac
KtorZ <matthias.benkort@gmail.com>
2021-06-22 20:28:32 +0200

    Adjust 'download-all' script to tweak cardano-config references to genesis files.

--- .github/scripts/download-all.sh
+++ .github/scripts/download-all.sh
@@ -5,13 +5,15 @@
 CARDANO_CONFIG_URL=$1
 CARDANO_NETWORK=$2
 
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-config.json            -O network/$CARDANO_NETWORK/cardano-node/config.json
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-topology.json          -O network/$CARDANO_NETWORK/cardano-node/topology.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-topology.json -O network/$CARDANO_NETWORK/cardano-node/topology.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-config.json -O network/$CARDANO_NETWORK/cardano-node/config.json
+sed -i 's@\("ByronGenesisFile"\):.*$@\1: "../genesis/byron.json",@' network/$CARDANO_NETWORK/cardano-node.config.json
+sed -i 's@\("ShelleyGenesisFile"\):.*$@\1: "../genesis/shelley.json",@' network/$CARDANO_NETWORK/cardano-node.config.json
+sed -i 's@\("AlonzoGenesisFile"\):.*$@\1: "../genesis/alonzo.json",@' network/$CARDANO_NETWORK/cardano-node.config.json
 
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-byron-genesis.json     -O network/$CARDANO_NETWORK/genesis/byron.json
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-shelley-genesis.json   -O network/$CARDANO_NETWORK/genesis/shelley.json
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-alonzo-genesis.json    -O network/$CARDANO_NETWORK/genesis/alonzo.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-byron-genesis.json -O network/$CARDANO_NETWORK/genesis/byron.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-shelley-genesis.json -O network/$CARDANO_NETWORK/genesis/shelley.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-alonzo-genesis.json -O network/$CARDANO_NETWORK/genesis/alonzo.json
 
-wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-db-sync-config.json    -O network/$CARDANO_NETWORK/cardano-db-sync/config.json
-# Somehow, the configuration for db-sync refers to some path in the Nix store :| ...
-sed -i 's@"NodeConfigFile":.*$@"NodeConfigFile": "../cardano-node/config.json",@' network/**/cardano-db-sync/config.json
+wget -q $CARDANO_CONFIG_URL/$CARDANO_NETWORK-db-sync-config.json -O network/$CARDANO_NETWORK/cardano-db-sync/config.json
+sed -i 's@\("NodeConfigFile"\):.*$@\1: "../cardano-node/config.json",@' network/$CARDANO_NETWORK/cardano-db-sync/config.json
